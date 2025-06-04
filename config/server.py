1de2ce975da573094ed301fa545b2087cf66e4e9
37r4n <e.37r4n@gmail.com>
2025-06-03 20:08:24 -0600

    style: space between variable name and type

--- config/server.py
+++ config/server.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Server:
-    port:int = int(os.getenv("PORT", 8080)) 
-    host:str = os.getenv("HOST", "127.0.0.1")
+    port: int = int(os.getenv("PORT", 8080)) 
+    host: str = os.getenv("HOST", "127.0.0.1")
 
 server = Server()
